7a14687613edc8515606adb98b5b9a6c20a2653c
Joseph Thibault <joe@cursivetechnology.com>
2026-02-02 17:05:10 -0500

    increment and version year correction

--- version.php
+++ version.php
@@ -25,10 +25,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_assignmentnotice';
-$plugin->version = 2025013100;
+$plugin->version = 2026020200;
 $plugin->requires = 2022112800; // Moodle 4.1.
 $plugin->maturity = MATURITY_BETA;
-$plugin->release = '0.2.0';
+$plugin->release = '0.3.0';
 $plugin->dependencies = [
     'mod_assign' => 2022112800,
 ];
